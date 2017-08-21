6c3d0732b8a5cbe216a5ed03b5e732afbd05a0a1
Christian <christiansuasi@gmail.com>
2017-08-20 22:56:53 -0400

    fixing dates

--- dates.js
+++ dates.js
@@ -3,8 +3,12 @@
 
 
 //nowDate displays current date
-var nowDate = new Date().toISOString();
-document.getElementById("nowDateString").innerHTML = nowDate.substring(0, nowDate.length-14);
+var presentDate = new Date().toISOString();
+document.getElementById("presentDateString").innerHTML = presentDate.substring(0, presentDate.length-14);
+
+//nowDate displays current date
+var currentDate = new Date("2000-09-04").toISOString();
+document.getElementById("currentDateString").innerHTML = currentDate.substring(0, currentDate.length-14);
 
 //nextDate displays date of next adventure
 var nextDate = new Date("2017-09-01").toISOString();
