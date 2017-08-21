d440fb2924c5582a83252559227578cc36f103c8
Christian <christiansuasi@gmail.com>
2017-08-20 23:06:09 -0400

    static dates

--- dates.js
+++ dates.js
@@ -2,14 +2,6 @@
 
 
 
-//nowDate displays current date
-var presentDate = new Date().toISOString();
-document.getElementById("presentDateString").innerHTML = presentDate.substring(0, presentDate.length-14);
-
-//nowDate displays current date
-var currentDate = new Date("2000-09-04").toISOString();
+//currentDate displays current date
+var currentDate = new Date().toISOString();
 document.getElementById("currentDateString").innerHTML = currentDate.substring(0, currentDate.length-14);
-
-//nextDate displays date of next adventure
-var nextDate = new Date("2017-09-01").toISOString();
-document.getElementById("nextDateString").innerHTML = nextDate.substring(0, nextDate.length-14);
